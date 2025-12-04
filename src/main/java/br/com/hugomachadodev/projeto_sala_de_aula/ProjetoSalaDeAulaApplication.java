fbd49fd0f4fc97e5d18095af423dd59b686f91ab
Hugo Machado <hugo_machado02@hotmail.com>
2025-12-04 01:54:05 -0300

    ✨ feat: Adiciona base do projeto

---
+++ src/main/java/br/com/hugomachadodev/projeto_sala_de_aula/ProjetoSalaDeAulaApplication.java
@@ -0,0 +1,13 @@
+package br.com.hugomachadodev.projeto_sala_de_aula;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSalaDeAulaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSalaDeAulaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/hugomachadodev/projeto_sala_de_aula/ProjetoSalaDeAulaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.hugomachadodev.projeto_sala_de_aula;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSalaDeAulaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
