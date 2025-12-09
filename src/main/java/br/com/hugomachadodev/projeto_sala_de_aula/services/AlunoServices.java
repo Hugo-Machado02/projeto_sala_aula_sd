73108aff9476a8307ddd58d71085fe3560f4c81d
Hugo Machado <hugo_machado02@hotmail.com>
2025-12-09 00:23:47 -0300

    ✨ feat: Implementa Logica de negócios do Aluno

--- src/main/java/br/com/hugomachadodev/projeto_sala_de_aula/services/AlunoServices.java
+++ src/main/java/br/com/hugomachadodev/projeto_sala_de_aula/services/AlunoServices.java
@@ -1,61 +1,50 @@
 package br.com.hugomachadodev.projeto_sala_de_aula.services;
-
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.hugomachadodev.projeto_sala_de_aula.exception.ResourceNotFoundException;
 import br.com.hugomachadodev.projeto_sala_de_aula.model.Aluno;
+import br.com.hugomachadodev.projeto_sala_de_aula.repositories.AlunoRepository;
 
 @Service
 public class AlunoServices {
 
-    public Aluno findbyId(String id){
-        Aluno aluno = new Aluno(
-            "Aluno da Silva - Busca por ID",
-            "10/10/2000",
-            "89783823234",
-            "teste.silva@estudante.ifgoiano.edu.br",
-            "63999999999"
-        );
+    @Autowired
+    AlunoRepository alunoRepository;
 
-        return aluno;
+
+    public Aluno findbyId(Long id){
+        return alunoRepository.findById(id).orElseThrow(()->new ResourceNotFoundException("Não Há Registros para esse ID"));
     }
 
     public List<Aluno> findAll(){
-        List<Aluno> alunos = new ArrayList<>();
-
-        for(int i = 0; i < 10; i++){
-            Aluno aluno = mockAlunos(i);
-            alunos.add(aluno);
-        }
-        return alunos;
+        return alunoRepository.findAll();
     }
 
     public Aluno create(Aluno aluno) {
-		return aluno;
+		return alunoRepository.save(aluno);
 	}
 
     public Aluno update(Aluno aluno) {
-		return aluno;
+        var entity = alunoRepository.findById(aluno.getIdAluno()).orElseThrow(
+            ()->new ResourceNotFoundException("Não Há Registros para esse ID"));
+
+        entity.setNomeCompleto(aluno.getNomeCompleto());
+        entity.setDataNascimento(aluno.getDataNascimento());
+        entity.setIdentificacao(aluno.getIdentificacao());
+        entity.setEmailInstitucional(aluno.getEmailInstitucional());
+        entity.setTelefone(aluno.getTelefone());
+
+		return alunoRepository.save(entity);
 	}
 
-    public Aluno delete(Aluno aluno) {
-		return aluno;
+    public void delete(Long id) {
+		var entity = alunoRepository.findById(id).orElseThrow(
+            ()->new ResourceNotFoundException("Não Há Registros para esse ID"));
+        
+        alunoRepository.delete(entity);
 	}
-
-
-
-    private Aluno mockAlunos(int i) {
-        Aluno aluno = new Aluno(
-            "Aluno da Silva "+ i +" - Busca por ID",
-            "10/10/2000",
-            "89783823234",
-            "teste.silva@estudante.ifgoiano.edu.br",
-            "63999999999"
-        );
-
-        return aluno;
-    }
 }
 
